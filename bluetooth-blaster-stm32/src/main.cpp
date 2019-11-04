fe75d4b8bd318f89d1f0c756fef17f24aeb42728
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2019-11-04 18:15:15 +0100

    Add cache for stm32 compilation

--- bluetooth-blaster-stm32/src/main.cpp
+++ bluetooth-blaster-stm32/src/main.cpp
@@ -3,8 +3,11 @@
 int main() {
 
   // put your setup code here, to run once:
+  printf("Hello!\n");
 
   while(1) {
     // put your main code here, to run repeatedly:
+    wait(1);
+    printf("World!\n");
   }
 }
