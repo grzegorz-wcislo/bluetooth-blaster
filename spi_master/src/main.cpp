6ad2b7c48daeaa44e758c8a72edbe8f43917ed06
h4di0x <h4di0x@localhost.localdomain>
2020-01-13 16:47:46 +0100

    WIP

--- spi_master/src/main.cpp
+++ spi_master/src/main.cpp
@@ -2,16 +2,95 @@
 #include <SPI.h>
 #include <BluetoothSerial.h>
 
-#define START_SIGNAL 1
+#define START_SIGNAL 0xCC
 #define TRANSFER_SIGNAL 0xff
-#define DATA_SIZE 325
+#define DATA_SIZE 330
 #define CRC16 0x8005
 #define BUFFER_SIZE DATA_SIZE + 7
-#define CORRECT_CRC 1
-#define INCORRECT_CRC 0
+
+uint8_t* buffer;
+
 
 bool end_transmission = false;
 BluetoothSerial SerialBT;
+uint8_t* get_buffer_from_SPI();
+uint16_t gen_crc16(const uint8_t *data, uint16_t size);
+void check_and_send_by_bluetooth(int crc, int crc_from_buffer, uint8_t* buffer);
+void get_valid_spi_frame();
+void get_spi_frame();
+bool valid_spi_frame();
+void send_bluetooth_buffer();
+
+void setup() {
+  Serial.begin(9600);
+  SerialBT.begin("bluetooth-blaster-esp32");
+  buffer = (uint8_t *) malloc(BUFFER_SIZE * sizeof(uint8_t));
+  SPI.begin(18,19,23,5);
+}
+
+void loop() {
+  Serial.printf("STARTING READ\n");
+  get_valid_spi_frame();
+  send_bluetooth_buffer();
+  // Serial.printf("%s\n", buffer);
+//   if(!end_transmission) {
+//     SPI.beginTransaction(SPISettings(1000, MSBFIRST, SPI_MODE0));
+//     uint8_t* buffer = get_buffer_from_SPI();
+//     uint8_t* buffer_cpy = (uint8_t *) malloc(BUFFER_SIZE * sizeof(uint8_t));
+//     memcpy(buffer_cpy, buffer, BUFFER_SIZE);
+//     uint16_t crc = gen_crc16(buffer_cpy, DATA_SIZE+4);
+//     uint16_t crc_from_buffer = buffer[DATA_SIZE + 5] | (buffer[DATA_SIZE + 5] << 8);
+//     check_and_send_by_bluetooth(crc, crc_from_buffer, buffer);
+//     free(buffer);
+//     free(buffer_cpy);
+//     SPI.endTransaction();
+//   }
+}
+
+uint8_t* get_buffer_from_SPI() {
+  uint8_t* buffer = (uint8_t *) malloc(BUFFER_SIZE * sizeof(uint8_t));
+    for(int i = 0; i< BUFFER_SIZE;i++) {
+          buffer[i] = (uint8_t) SPI.transfer(TRANSFER_SIGNAL);
+          Serial.printf("%c", buffer[i]);
+    }
+    return buffer;
+}
+
+void check_and_send_by_bluetooth(int crc, int crc_from_buffer, uint8_t* buffer) {
+      if(crc == crc_from_buffer) {
+      if(buffer[0] == 1) {
+        end_transmission = true;
+      }
+      Serial.printf("%s", "\nCRC CORRECT\n");
+      SerialBT.write(buffer, BUFFER_SIZE);
+    }
+}
+
+void get_valid_spi_frame() {
+  do {
+    get_spi_frame();
+    Serial.printf("\nFRAME GOT\n");
+  } while(!valid_spi_frame());
+  Serial.printf("\nFRAME VALID\n");
+}
+
+void get_spi_frame() {
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+  SPI.transfer(START_SIGNAL);
+  for (int i = 0; i < BUFFER_SIZE; i++) {
+    buffer[i] = SPI.transfer(0);
+  }
+  SPI.endTransaction();
+}
+
+bool valid_spi_frame() {
+  return true;
+}
+
+void send_bluetooth_buffer() {
+  SerialBT.write(buffer, BUFFER_SIZE);
+  Serial.write(buffer, BUFFER_SIZE);
+}
 
 uint16_t gen_crc16(const uint8_t *data, uint16_t size)
 {
@@ -50,48 +129,4 @@ uint16_t gen_crc16(const uint8_t *data, uint16_t size)
         if (i & out) crc |= j;
     }
     return crc;
-}
-
-void setup() {
-  Serial.begin(9600);
-  SerialBT.begin("bluetooth-blaster-esp32");
-
-  SPI.begin(18,19,23,5);
-  SPI.beginTransaction(SPISettings(1000, MSBFIRST, SPI_MODE0));
-  SPI.transfer(START_SIGNAL);
-  SPI.endTransaction();
-}
-
-void loop() {
-
-  if(!end_transmission) {
-    SPI.beginTransaction(SPISettings(1000, MSBFIRST, SPI_MODE0));
-    uint8_t* buffer = (uint8_t *) malloc(BUFFER_SIZE * sizeof(uint8_t));
-    for(int i = 0; i< BUFFER_SIZE;i++) {
-          buffer[i] = (uint8_t) SPI.transfer(TRANSFER_SIGNAL);
-          Serial.printf("%c", buffer[i]);
-    }
-    uint8_t* buffer_cpy = (uint8_t *) malloc(BUFFER_SIZE * sizeof(uint8_t));
-    memcpy(buffer_cpy, buffer, BUFFER_SIZE);
-    uint16_t crc = gen_crc16(buffer_cpy, DATA_SIZE+4);
-    uint16_t crc_from_buffer = buffer[DATA_SIZE + 5] | (buffer[DATA_SIZE + 5] << 8);
-    if(crc == crc_from_buffer) {
-      SPI.transfer(CORRECT_CRC);
-      if(buffer[0] == 1) {
-        end_transmission = true;
-      }
-      SerialBT.write(buffer, BUFFER_SIZE);
-    } else {
-      SPI.transfer(INCORRECT_CRC);
-    }
-    free(buffer);
-    free(buffer_cpy);
-    SPI.endTransaction();
-  }
-
-  //   for (char i = 'a'; i <= 'z'; i++) {
-  //   Serial.printf("%c => %d\n", i, (char) SPI.transfer(i));
-  // }
-
-
 }
--- spi_slave/src/main.cpp
+++ spi_slave/src/main.cpp
@@ -4,10 +4,13 @@
 #define CRC16 0x8005
 #define BUFFER_SIZE DATA_SIZE + 7
 #define CORRECT_CRC 1
+#define START_SIGNAL 0xCC
 
 Serial pc(USBTX,USBRX);
 
 SPISlave device(PA_7, PA_6, PA_5, PA_15);
+
+void send_packet();
 
 uint16_t gen_crc16(const uint8_t *data, uint16_t size)
 {
@@ -49,66 +52,64 @@ uint16_t gen_crc16(const uint8_t *data, uint16_t size)
 }
 
 int main() {
-  srand(time(NULL));
-  pc.baud(9600);
-  device.format(8, 0);
-  device.reply(0x00);
-  device.frequency(1000);
-
-  bool start_command_recieved = false;
-  uint32_t frame_counter = 0;
-  pc.printf("WAITING FOR A DEVICE\n");
-  while(1) {
-    if(device.receive()) {
-      pc.printf("STARTING TO SEND DATA\n");
-      start_command_recieved = true;
+  device.frequency(1000000);
+  while (true) {
+    if (device.receive()) {
+      if (device.read() == START_SIGNAL)
+        send_packet();
     }
-
-    if(start_command_recieved) {
-      for(int j = 0;j<200;j++) {
-         uint8_t* buffer = (uint8_t *) malloc(BUFFER_SIZE * sizeof(uint8_t));
-         if(j == 199) {
-          buffer[0] = (uint8_t) 1;
-         } else {
-           buffer[0] = (uint8_t) 0;
-         }
-        for(int i = 1; i <= DATA_SIZE; i++) {
-          buffer[i] = (uint8_t) (rand()%26 + 'a');
-        }
-        buffer[DATA_SIZE + 1] = frame_counter;
-        buffer[DATA_SIZE + 2] = (frame_counter >> 8);
-        buffer[DATA_SIZE + 3] = (frame_counter >> 16);
-        buffer[DATA_SIZE + 4] = (frame_counter >> 24);
-        frame_counter++;
-        uint8_t* buffer_cpy = (uint8_t *) malloc(BUFFER_SIZE * sizeof(uint8_t));
-        memcpy(buffer_cpy, buffer, BUFFER_SIZE);
-        uint16_t crc = gen_crc16(buffer_cpy, DATA_SIZE+4);
-        buffer[DATA_SIZE + 5] = crc;
-        buffer[DATA_SIZE + 6] = (crc >> 8);
-        bool sent_correctly = false;
-        pc.printf("IS LAST FRAME: ");
-        pc.printf("%d", buffer[0]);
-        pc.printf("\nDATA SENT: ");
-        for(int i = 1; i <= DATA_SIZE;i++) {
-          pc.printf("%c", buffer[i]);
-        }
-        while(!sent_correctly) {
-          for(int i = 0; i< BUFFER_SIZE; i++) {
-            pc.printf("Waiting for reply\n");
-            device.reply((int) buffer[i]);
-          }
-          while(1) {
-            if(device.receive() && device.read() == CORRECT_CRC) {
-              sent_correctly = true;
-            }
-          }
-        }
-        free(buffer);
-        free(buffer_cpy);
-      }
-    }
-
   }
+  // srand(time(NULL));
+  // pc.baud(9600);
+  // device.format(8, 0);
+  // device.reply(0x00);
+  // device.frequency(1000);
+
+  // bool start_command_recieved = false;
+  // uint32_t frame_counter = 0;
+  // pc.printf("WAITING FOR A DEVICE\n");
+  // while(1) {
+  //   if(device.receive()) {
+  //     pc.printf("STARTING TO SEND DATA\n");
+  //     start_command_recieved = true;
+  //   }
+
+  //   if(start_command_recieved) {
+  //     for(int j = 0;j<200;j++) {
+  //        uint8_t* buffer = (uint8_t *) malloc(BUFFER_SIZE * sizeof(uint8_t));
+  //        if(j == 199) {
+  //         buffer[0] = (uint8_t) 1;
+  //        } else {
+  //          buffer[0] = (uint8_t) 0;
+  //        }
+  //       for(int i = 1; i <= DATA_SIZE; i++) {
+  //         buffer[i] = (uint8_t) (rand()%26 + 'a');
+  //       }
+  //       buffer[DATA_SIZE + 1] = frame_counter;
+  //       buffer[DATA_SIZE + 2] = (frame_counter >> 8);
+  //       buffer[DATA_SIZE + 3] = (frame_counter >> 16);
+  //       buffer[DATA_SIZE + 4] = (frame_counter >> 24);
+  //       frame_counter++;
+  //       uint8_t* buffer_cpy = (uint8_t *) malloc(BUFFER_SIZE * sizeof(uint8_t));
+  //       memcpy(buffer_cpy, buffer, BUFFER_SIZE);
+  //       uint16_t crc = gen_crc16(buffer_cpy, DATA_SIZE+4);
+  //       buffer[DATA_SIZE + 5] = crc;
+  //       buffer[DATA_SIZE + 6] = (crc >> 8);
+  //       pc.printf("IS LAST FRAME: ");
+  //       pc.printf("%d", buffer[0]);
+  //       pc.printf("\nDATA SENT: ");
+  //       for(int i = 1; i <= DATA_SIZE;i++) {
+  //         pc.printf("%c", buffer[i]);
+  //       }
+  //         for(int i = 0; i< BUFFER_SIZE; i++) {
+  //           device.reply((int) buffer[i]);
+  //         }
+  //       free(buffer);
+  //       free(buffer_cpy);
+  //     }
+  //   }
+
+  // }
 
 //  char buff[100];
 //   int i = 0;
@@ -127,4 +128,10 @@ int main() {
   //     i = 0;
   //   }
   // }
+}
+
+void send_packet() {
+  for(int i = 0;i<337;i++) {
+    device.reply('5');
+  }
 }
